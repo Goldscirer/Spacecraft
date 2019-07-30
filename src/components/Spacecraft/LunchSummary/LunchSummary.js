c59656f25c306e357632950758cbeeeed77b0661
Seweryn <seweryn.wawrzynowicz@gmail.com>
2019-07-30 20:57:57 +0200

    Added content to LunchSummary

---
+++ src/components/Spacecraft/LunchSummary/LunchSummary.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Aux from './../../../hoc/ReactAux'
+
+const lunchSummary = (props) => {
+    const partsSummary = props.parts
+    return (
+        <Aux>
+            <h3>Your lanch details:</h3>
+        </Aux>
+    )
+};
+
+export default lunchSummary;
